82750eeac31a37a229954bd7625d023e12fbd21e
vcalvo25 <victoria.calvo1013@hotmail.com>
2025-01-31 20:54:48 -0500

    finalized javascript and added manifest file

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
-const ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads  = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url) 
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+    `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
+    inputEl.value = ""
 
-for (let i = 0; i < myLeads.length; i++) {
-   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-}
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
